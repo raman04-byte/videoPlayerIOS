c9de559d75a3c73fb349865f75687708a7d29667
Raman Tank <ramantank04022002@gmail.com>
2024-10-03 14:46:44 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Raman Tank on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTests.swift
+//  VideoPlayerTests
+//
+//  Created by Raman Tank on 03/10/24.
+//
+
+import Testing
+@testable import VideoPlayer
+
+struct VideoPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
